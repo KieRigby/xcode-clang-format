42a6ce135e89f0c99a70c64cbfd8384316b9d710
Kieran Rigby <kieran.rigby@focusrite.com>
2021-11-18 00:07:31 +0000

    Add xcode project

---
+++ xcode-clang-format/xcode-clang-format/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  xcode-clang-format
+//
+//  Created by Kieran Rigby on 18/11/2021.
+//  Copyright © 2021 Kieran Rigby. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcode-clang-format/xcode-clang-formatTests/xcode_clang_formatTests.swift
@@ -0,0 +1,34 @@
+//
+//  xcode_clang_formatTests.swift
+//  xcode-clang-formatTests
+//
+//  Created by Kieran Rigby on 18/11/2021.
+//  Copyright © 2021 Kieran Rigby. All rights reserved.
+//
+
+import XCTest
+@testable import xcode_clang_format
+
+class xcode_clang_formatTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
